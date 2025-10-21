da2bd6bc3256786b745d3bd22768e1503f06ecd9
Fahri Kutluoğlu <fahri.kutluoglu@turktelekom.com.tr>
2025-10-21 08:03:12 +0300

    20251020__ PrayerDataTestSuite runs and passed all test on Prayer Data Module...

---
+++ prayer/data/src/test/java/com/kutluoglu/prayer/data/PrayerDataTestSuite.kt
@@ -0,0 +1,15 @@
+package com.kutluoglu.prayer.data
+
+import org.junit.platform.suite.api.SelectPackages
+import org.junit.platform.suite.api.Suite
+import org.junit.platform.suite.api.SuiteDisplayName
+
+/**
+ * Created by F.K. on 21.10.2025.
+ *
+ */
+
+@Suite
+@SuiteDisplayName("Prayer Data Test Suite")
+@SelectPackages("com.kutluoglu.prayer.data")
+class PrayerDataTestSuite
