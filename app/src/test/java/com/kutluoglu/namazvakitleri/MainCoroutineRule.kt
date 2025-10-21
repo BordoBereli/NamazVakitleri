83d2a65dc1fb01fc3b6d02315e60af084bf52760
Fahri Kutluoğlu <fahri.kutluoglu@turktelekom.com.tr>
2025-10-21 09:19:04 +0300

    20251020__ JUnit4 Test Rule annotation is replaced with JUnit5 ExtendWith annotation...

--- app/src/test/java/com/kutluoglu/namazvakitleri/MainCoroutineRule.kt
+++ app/src/test/java/com/kutluoglu/namazvakitleri/MainCoroutineRule.kt
@@ -6,6 +6,9 @@ import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
+import org.junit.jupiter.api.extension.AfterEachCallback
+import org.junit.jupiter.api.extension.BeforeEachCallback
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
@@ -13,12 +16,13 @@ import org.junit.runner.Description
 @ExperimentalCoroutinesApi
 class MainCoroutineRule(
     private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
-) : TestWatcher() {
-    override fun starting(description: Description) {
-        Dispatchers.setMain(testDispatcher)
+) : AfterEachCallback, BeforeEachCallback {
+
+    override fun afterEach(context: ExtensionContext?) {
+        Dispatchers.resetMain()
     }
 
-    override fun finished(description: Description) {
-        Dispatchers.resetMain()
+    override fun beforeEach(context: ExtensionContext?) {
+        Dispatchers.setMain(testDispatcher)
     }
 }
