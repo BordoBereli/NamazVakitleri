d05aae8ba1267ede15bd410c31627cf804fc68b1
Fahri Kutluoğlu <fahri.kutluoglu@turktelekom.com.tr>
2025-10-21 10:56:02 +0300

    20251020__ Prayer Domain module unit tests are moved to under com.kutluoglu.prayer.domain package...

---
+++ app/src/test/java/com/kutluoglu/namazvakitleri/AppTestSuite.kt
@@ -0,0 +1,15 @@
+package com.kutluoglu.namazvakitleri
+
+import org.junit.platform.suite.api.SelectPackages
+import org.junit.platform.suite.api.Suite
+import org.junit.platform.suite.api.SuiteDisplayName
+
+/**
+ * Created by F.K. on 21.10.2025.
+ *
+ */
+
+@Suite
+@SuiteDisplayName("App Test Suite")
+@SelectPackages("com.kutluoglu.namazvakitleri")
+class AppTestSuite
