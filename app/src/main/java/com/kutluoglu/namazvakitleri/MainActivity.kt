8c417e8c8761bf18386f32d939ea4729bd3ccb6c
Fahri Kutluoğlu <fahri.kutluoglu@turktelekom.com.tr>
2026-08-17 22:19:42 +0300

    feat: install splash screen on app start

--- app/src/main/java/com/kutluoglu/namazvakitleri/MainActivity.kt
+++ app/src/main/java/com/kutluoglu/namazvakitleri/MainActivity.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.core.view.WindowCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.kutluoglu.core.designsystem.theme.NamazVakitleriTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
