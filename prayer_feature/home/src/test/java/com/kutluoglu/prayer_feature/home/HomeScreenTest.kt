0ed2d6d6969dfb0d90f9b921c656c1729ca7e0b2
Fahri Kutluoğlu <fahri.kutluoglu@turktelekom.com.tr>
2026-08-22 18:33:42 +0300

    test(home): add Compose UI smoke test

---
+++ prayer_feature/home/src/test/java/com/kutluoglu/prayer_feature/home/HomeScreenTest.kt
@@ -0,0 +1,48 @@
+package com.kutluoglu.prayer_feature.home
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.navigation.NavController
+import com.kutluoglu.prayer.model.location.LocationData
+import com.kutluoglu.prayer.model.location.LocationEntry
+import com.kutluoglu.prayer_feature.home.common.QuranVerseFormatter
+import com.kutluoglu.prayer_feature.home.state.HomeUiState
+import com.kutluoglu.prayer_location.data.LocationsState
+import io.mockk.mockk
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
+class HomeScreenTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    private val istanbul = LocationEntry(
+        id = "loc-1",
+        location = LocationData(41.0082, 28.9784, "Turkey", "TR", "Istanbul", null),
+        displayName = "Istanbul, Turkey"
+    )
+
+    @Test
+    fun `renders error state with retry`() {
+        composeTestRule.setContent {
+            HomeScreen(
+                navController = mockk<NavController>(relaxed = true),
+                uiState = HomeUiState.Error("Something went wrong"),
+                locationsState = LocationsState(entries = listOf(istanbul), selectedId = "loc-1"),
+                prayerDataByLocation = emptyMap(),
+                activeLocationId = "loc-1",
+                quranVerseFormatter = mockk<QuranVerseFormatter>(relaxed = true),
+                onEvent = {}
+            )
+        }
+        composeTestRule.onNodeWithText("Something went wrong").assertIsDisplayed()
+    }
+}
