8d966a3e06f89b7195f78ae77eb51a4250217ce4
Fahri Kutluoğlu <fahri.kutluoglu@turktelekom.com.tr>
2026-09-02 10:50:32 +0300

    feat(widget): register widget components in koin di module

---
+++ prayer_widget/src/main/java/com/kutluoglu/prayer_widget/di/PrayerWidgetModule.kt
@@ -0,0 +1,10 @@
+package com.kutluoglu.prayer_widget.di
+
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Configuration
+import org.koin.core.annotation.Module
+
+@Module
+@Configuration
+@ComponentScan("com.kutluoglu.prayer_widget**")
+object PrayerWidgetModule
