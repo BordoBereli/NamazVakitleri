8a7ea94f8bba7ef2cdb050b78b5b406182916b11
Fahri Kutluoğlu <fahri.kutluoglu@turktelekom.com.tr>
2026-09-10 10:47:05 +0300

    style(settings): order juristic screen alphabetically grouped imports

--- prayer_feature/settings/src/main/java/com/kutluoglu/prayer_feature/settings/juristic/JuristicMethodScreen.kt
+++ prayer_feature/settings/src/main/java/com/kutluoglu/prayer_feature/settings/juristic/JuristicMethodScreen.kt
@@ -1,5 +1,6 @@
 package com.kutluoglu.prayer_feature.settings.juristic
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -31,7 +32,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.annotation.StringRes
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.kutluoglu.core.designsystem.R
